11a43a511c3a2625006a3a389439aeb3db209cbe
Glen Simon <glen.a.simon@gmail.com>
2024-10-28 12:29:07 -0400

    Add explicit breakpoints

--- theme.ts
+++ theme.ts
@@ -1,9 +1,8 @@
 import { createTheme, colorsTuple } from "@mantine/core";
 
 export const theme = createTheme({
-   primaryColor: "gray",
+   primaryColor: "blue",
    colors: {
-      
       "ocean-blue": [
          "#7AD1DD",
          "#5FCCDB",
@@ -17,5 +16,12 @@ export const theme = createTheme({
          "#147885",
       ],
       glass: colorsTuple("rgba(40,40,40,0.92)"),
+   },
+   breakpoints: {
+      xs: "36em",
+      sm: "48em",
+      md: "62em",
+      lg: "75em",
+      xl: "88em",
    },
 });
