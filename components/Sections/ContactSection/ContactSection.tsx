ea31ada751d29bdb0df5a23d0f4eb4969abb27e3
Glen Simon <glen.a.simon@gmail.com>
2024-11-14 20:30:39 -0500

    Add contact section

--- components/Sections/ContactSection/ContactSection.tsx
+++ components/Sections/ContactSection/ContactSection.tsx
@@ -1,17 +1,58 @@
-import { Container } from "@mantine/core";
+import { ActionIcon, Center, Container, Group } from "@mantine/core";
 import React from "react";
 import classes from "./ContactSection.module.css";
 import { ElementIds } from "../../../constants";
+import {
+   IconMail,
+   IconBrandGithub,
+   IconBrandLinkedin,
+} from "@tabler/icons-react";
+import { notifications } from "@mantine/notifications";
 
 const ContactSection: React.FC = () => {
+   const variant = "outline";
+   const size = "xl";
+
+   const handleClick = () => {
+      navigator.clipboard.writeText("glen.a.simon@gmail.com");
+      notifications.show({
+         title: "Email Copied",
+         message: "Email copied to clipboard",
+      });
+   };
+
    return (
-      <Container
-         fluid
-         className="section-container"
-         h={1000}
-         id={ElementIds.Contact}
-      >
+      <Container fluid className="section-container" id={ElementIds.Contact}>
          <h2>Contact</h2>
+         <Center>
+            <Group>
+               <ActionIcon
+                  component="a"
+                  href="mailto:glen.a.simon@gmail.com"
+                  onClick={handleClick}
+                  variant={variant}
+                  size={size}
+               >
+                  <IconMail />
+               </ActionIcon>
+               <ActionIcon
+                  component="a"
+                  href="https://github.com/gsimon2"
+                  variant={variant}
+                  size={size}
+               >
+                  <IconBrandGithub />
+               </ActionIcon>
+               <ActionIcon
+                  component="a"
+                  href="https://www.linkedin.com/in/glen-a-simon/"
+                  variant={variant}
+                  size={size}
+               >
+                  <IconBrandLinkedin />
+               </ActionIcon>
+            </Group>
+         </Center>
       </Container>
    );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,13 @@ import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "../theme";
 import "../global.css";
+import '@mantine/notifications/styles.css';
+import { Notifications } from '@mantine/notifications';
 
 export default function App({ Component, pageProps }: any) {
    return (
       <MantineProvider theme={theme} defaultColorScheme="dark">
+         <Notifications />
          <Component {...pageProps} />
       </MantineProvider>
    );
