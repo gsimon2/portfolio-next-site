0b433f9fdd59f6705453fcc091ac818f0ee25d97
Glen Simon <glen.a.simon@gmail.com>
2024-11-14 20:00:59 -0500

    Update about me section

--- components/Sections/AboutSection/AboutSection.tsx
+++ components/Sections/AboutSection/AboutSection.tsx
@@ -1,8 +1,8 @@
 import { Center, Container, SimpleGrid, Space, Text } from "@mantine/core";
 import React from "react";
 import classes from "./AboutSection.module.css";
-import SkillsDisplay from './SkillsDisplay';
-import { ElementIds } from '../../../constants';
+import SkillsDisplay from "./SkillsDisplay";
+import { ElementIds } from "../../../constants";
 
 const AboutSection: React.FC = () => {
    return (
@@ -11,29 +11,32 @@ const AboutSection: React.FC = () => {
          <SimpleGrid cols={{ base: 1, sm: 2 }} className={classes.grid}>
             <Center className={classes.textContainer}>
                <Text>
-                  Hey, I&apos;m Glen! Currently I am working as a Cloud Software
-                  Engineer at Indeed. I really enjoy creating new web
-                  applications and expanding current ones to make our customers’
-                  lives a little easier. I find that taking even a small idea
-                  and bringing it to fruition is very exciting and allows me to
-                  be passionate about my work. My goal is to be able to chase
-                  this passion with more significant and impactful projects
-                  until I can help provide millions of people with something
-                  that they truly enjoy using.
+                  Hey, I&apos;m Glen! I&apos;m currently crafting cloud
+                  solutions as a Software Engineer at Indeed, where I get to
+                  build and enhance web applications that make a real difference
+                  in people&apos;s lives. What drives me is taking ideas from
+                  concept to reality, whether it&apos;s developing enterprise
+                  solutions at work or creating custom modules for the Foundry
+                  VTT community. I love seeing how my code can enhance
+                  others&apos; experiences, especially when it helps fellow
+                  gamers dive deeper into their adventures.
                </Text>
                <Space h="lg" />
                <Text>
-                  I live in Portland, Michigan with my beautiful fiancé Mae and
-                  our pupper Loki. In my spare time, I enjoy hiking, kayaking,
-                  and playing sports. When I am not outside, you can generally
-                  find me on the computer programming small projects, prepping
-                  role-playing adventures for my friends, or playing video
-                  games.
+                  When I&apos;m not coding, you&apos;ll find me on the mats
+                  practicing Brazilian Jiu-Jitsu, hitting the trails for a run,
+                  or planning my next camping expedition. I&apos;m based in
+                  Portland, Michigan, where I live with my amazing wife Mae, our
+                  son Wesley, and our adventurous pup Loki. My evenings are
+                  often split between lifting weights, leveling up in video
+                  games, and running Pathfinder campaigns for my friends. This
+                  mix of physical activity, creative storytelling, and technical
+                  problem-solving keeps me energized and brings fresh
+                  perspectives to my development work.
                </Text>
-               <Space h="lg" />
             </Center>
             <Center>
-              <SkillsDisplay />
+               <SkillsDisplay />
             </Center>
          </SimpleGrid>
       </Container>
