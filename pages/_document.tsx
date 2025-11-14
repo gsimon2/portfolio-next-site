10d94ee44c45711d1a6f7dfe0bcf9709c2b57e07
Glen Simon <glen.a.simon@gmail.com>
2025-11-13 21:51:48 -0500

    Fix viewport meta tag being on server rendered component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -42,10 +42,6 @@ export default function Document() {
 
             {/* <!-- Additional SEO --> */}
             <meta name="robots" content="index, follow" />
-            <meta
-               name="viewport"
-               content="width=device-width, initial-scale=1.0"
-            />
             <link rel="canonical" href="https://www.glenasimon.com" />
             <link rel="shortcut icon" href="/favicon.png" />
             <link
